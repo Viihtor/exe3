429f4902143184093c79c789716021a0b6ed0d97
Victor Berzotti <victor_berzotti@hotmail.com>
2019-03-07 23:58:20 -0300

    first commit

---
+++ ContaDeBar/ContaDeBar/ContaBar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaDeBar
+{
+    public class ContaBar
+    {
+        public decimal Total;
+        public int QtdPessoas;
+        public decimal PorcGarcom;
+        public decimal TotalPessoa;
+        public decimal ValorUnitario;
+        public int Quantidade;
+
+
+        public ContaBar()
+        {
+            Total = 0;
+            QtdPessoas = 1;
+            PorcGarcom = 0.10M;
+        }
+
+        public void AdicionaCompra(bool garcom)
+        {     
+            if(garcom == true)
+            {
+                Total += (ValorUnitario * Quantidade) / QtdPessoas;
+                Math.Round(Total += (Total * PorcGarcom), 2);
+            }   
+            else
+            {
+                Math.Round(Total += (ValorUnitario * Quantidade) / QtdPessoas, 2);
+            }
+
+
+        }
+
+        public void CalculoPorPessoa()
+        {
+            Math.Round(TotalPessoa = Total / QtdPessoas, 2);
+        }
+
+    }
+
+}
---
+++ ContaDeBar/ContaDeBar/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContaDeBar
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        ContaBar contaBar = new ContaBar();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDespesa_Click(object sender, RoutedEventArgs e)
+        {
+            contaBar.ValorUnitario = Convert.ToDecimal(txtValorUnitario.Text);
+            contaBar.Quantidade = Convert.ToInt32(txtQuantidade.Text);
+            contaBar.QtdPessoas = Convert.ToInt32(txtQtdPessoas.Text);
+            bool garcom = Convert.ToBoolean(ckGarcom.IsChecked);
+
+            contaBar.AdicionaCompra(garcom);
+            contaBar.CalculoPorPessoa();
+
+            lblTotal.Content = contaBar.Total.ToString();
+            lblTotalPessoa.Content = contaBar.TotalPessoa.ToString();
+
+            txtQuantidade.Clear();
+            txtValorUnitario.Clear();
+        }
+    }
+}
